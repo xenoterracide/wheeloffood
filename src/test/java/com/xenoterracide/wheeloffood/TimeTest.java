faea4832ddf2278785aaa68c6f1e95a72100333c
Caleb Cushing <xenoterracide@gmail.com>
2013-06-03 08:07:30 -0500

    remove unused import
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- src/test/java/com/xenoterracide/wheeloffood/TimeTest.java
+++ src/test/java/com/xenoterracide/wheeloffood/TimeTest.java
@@ -3,7 +3,6 @@ package com.xenoterracide.wheeloffood;
 
 import org.junit.*;
 import static org.junit.Assert.*;
-import org.junit.matchers.JUnitMatchers;
 import org.junit.rules.ExpectedException;
 
 public class TimeTest {
